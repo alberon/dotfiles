476abddad7d74b9ffb93fa137fc530d74bfd1637
Dave James Miller <dave@davejamesmiller.com>
2011-06-09 14:36:42 +0100

    Fix hg prompt conditional load

--- .hgextensions/hg-prompt/conditional-load.py
+++ .hgextensions/hg-prompt/conditional-load.py
@@ -1,7 +1,8 @@
+import os
 import sys
 
 req_version = (2,5)
 cur_version = sys.version_info
 
 if cur_version[0] > req_version[0] or (cur_version[0] == req_version[0] and cur_version[1] >= req_version[1]):
-    import prompt
+    execfile(os.path.join(os.path.dirname(__file__), 'prompt.py'))
