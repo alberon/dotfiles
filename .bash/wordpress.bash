0f39a85c62eff0d4d4135b505d83d7045be7a3c1
Dave James Miller <dave@davejamesmiller.com>
2012-03-09 11:46:49 +0000

    Add cwt function (cd WordPress Theme)

--- .bash/wordpress.bash
+++ .bash/wordpress.bash
@@ -1,15 +1,38 @@
+function find_wordpress_wp_content {
+    if [ -d wp-content ]; then
+        echo wp-content
+    elif [ -d www/wp-content ]; then
+        echo www/wp-content
+    elif [ -d ../wp-content ]; then
+        echo ../wp-content
+    else
+        echo "Cannot find wp-content/ directory" >&2
+        return 1
+    fi
+}
+
 # cwc = "cd wp-content"
 # This is because it's very hard to tab-complete "wp-content" because you have
 # to type "wp-cont" before you get to a non-ambiguous prefix
 function cwc {
-    if [ -d wp-content ]; then
-        c wp-content
-    elif [ -d www/wp-content ]; then
-        c www/wp-content
-    elif [ -d ../wp-content ]; then
-        c ../wp-content
+    wp_root=$(find_wordpress_wp_content) || return
+    c $wp_root
+}
+
+# cwt = "cd WordPress Theme"
+function cwt {
+    wp_root=$(find_wordpress_wp_content) || return
+    if [ -d $wp_root/themes ]; then
+        wp_theme=$(find $wp_root/themes -mindepth 1 -maxdepth 1 -type d -not -name twentyten -not -name twentyeleven)
+        if [ $(echo "$possible_themes" | wc -l) -eq 1 ]; then
+            # Only 1 non-default theme found - assume we want that
+            c $wp_theme
+        else
+            # 0 or 2+ themes found - go to the main directory
+            c $wp_root/themes
+        fi
     else
-        echo "Cannot find wp-content/ directory" >&2
+        echo "Cannot find wp-content/themes/ directory" >&2
         return 1
     fi
 }
