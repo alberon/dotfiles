69bf8c1483b4a8a2a2b56714dd64e76a085583aa
Dave James Miller <dave@davejamesmiller.com>
2012-03-19 19:33:50 +0000

    Fix auto-update when not in home directory

--- .bash/auto-update.bash
+++ .bash/auto-update.bash
@@ -1,10 +1,10 @@
 # Check what the current revision is
-old_head=$(git rev-parse HEAD)
+old_head=$(cd; git rev-parse HEAD)
 
 # Run the auto-update
 ~/bin/cfg-auto-update
 
 # Reload Bash if any changes were made
-if [ "$(git rev-parse HEAD)" != "$old_head" ]; then
+if [ "$(cd; git rev-parse HEAD)" != "$old_head" ]; then
     exec bash -l
 fi
