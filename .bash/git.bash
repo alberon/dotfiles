4fa3ca76ea30070b8c6ce8b4db1c7e7f8395d661
Dave James Miller <dave@davejamesmiller.com>
2012-03-26 22:41:35 +0100

    Use git-repl by default when typing 'git' (or 'g' or 'cfg') with no command

--- .bash/git.bash
+++ .bash/git.bash
@@ -1,6 +1,16 @@
 # g = git
 alias g='git'
 
+# git with no parameters to load REPL interactive session
+function git {
+    if [ $# -gt 0 ]; then
+        env git "$@"
+    else
+        env git status &&
+        env git repl
+    fi
+}
+
 # Auto-complete git commands for git aliases (g, cfg, hub)
 if type _git >/dev/null 2>&1; then
     for cmd in g cfg hub; do
