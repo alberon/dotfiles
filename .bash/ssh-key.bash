c59f57e8eb86a933c907bd93a21c306a3459f3b9
Dave James Miller <dave@davejamesmiller.com>
2013-03-28 20:19:19 +0000

    Stop clearing screen before and after connecting to SSH server
    This reverts commit fce2825ab207d96c1476dcfd8f1c4711b30812e7.

--- .bash/ssh-key.bash
+++ .bash/ssh-key.bash
@@ -1,4 +1,3 @@
-# SSH key
 file=
 
 if ! $MAC; then
@@ -46,11 +45,3 @@ if [ -f "$file" ]; then
 
     fi
 fi
-
-# Clear screen before and after connecting
-# Not for security - more so I don't think I'm connected to the wrong server!
-function ssh {
-    clear
-    command ssh "$@"
-    clear
-}
