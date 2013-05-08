a7864568d1271bba69171d29e3c58755eeea6493
Dave James Miller <dave@davejamesmiller.com>
2013-05-08 22:34:04 +0100

    Set username for 'hub', and alias git=hub

--- .bash/git.bash
+++ .bash/git.bash
@@ -1,14 +1,43 @@
 # g = git
 alias g='git'
 
-# git with no parameters to load REPL interactive session
-git() {
-    if [ $# -gt 0 ]; then
-        env git "$@"
-    else
-        env git status &&
-        env git repl
+# Make sure hub username is configured
+# https://github.com/defunkt/hub/issues/225
+init_hub() {
+    if [ ! -f ~/.config/hub ]; then
+        if [ ! -d ~/.config ]; then
+            mkdir ~/.config
+        fi
+
+        cat >~/.config/hub <<END
+---
+github.com:
+- user: davejamesmiller
+END
     fi
+}
+
+git() {
+    # Alias to hub if available
+    if which ruby >/dev/null 2>&1; then
+        git=hub
+        init_hub
+    else
+        git=git
+    fi
+
+    # 'git' with no parameters loads interactive REPL
+    if [ $# -gt 0 ]; then
+        command $git "$@"
+    else
+        command $git status &&
+        command $git repl
+    fi
+}
+
+hub() {
+    init_hub
+    git "$@"
 }
 
 # Auto-complete git commands for git aliases (g, cfg, hub)
--- .bash/prompt.bash
+++ .bash/prompt.bash
@@ -46,12 +46,13 @@ if $HAS_TERMINAL; then
             fi
 
             # Show the branch name / tag / id
-            branch=`git branch --no-color 2>/dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
+            # Note: Using 'command git' to bypass 'hub' which is slightly slower and not needed here
+            branch=`command git branch --no-color 2>/dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
             if [ -n "$branch" -a "$branch" != "(no branch)" ]; then
                 echo -e "\033[30;1m on \033[35;1m$branch \033[0m(git)\033[30;1m"
                 #        ^grey       ^pink           ^light grey  ^ grey
             else
-                tag=`git describe --always 2>/dev/null`
+                tag=`command git describe --always 2>/dev/null`
                 if [ -z "$tag" ]; then
                     tag="(unknown)"
                 fi
