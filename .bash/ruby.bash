d0e941978cc2318db57c6b3827cbf606d89a12c7
Dave James Miller <dave@davejamesmiller.com>
2013-08-14 15:00:34 +0100

    Fix 'cd' command when latest version of RVM is installed

--- .bash/ruby.bash
+++ .bash/ruby.bash
@@ -1,6 +1,8 @@
 if $HAS_TERMINAL; then
 
     # rvm
+    rvm_project_rvmrc=0 # RVM 1.22.1 breaks my 'cd' alias, and I don't need this
+
     [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 
     # Use bundle exec to run these Rails commands
