c804a3d88ea6a14194a6a15422ea3b97068cae0c
Dave James Miller <dave@davejamesmiller.com>
2014-04-01 21:32:20 +0100

    Remove bundle exec prefix from cap (and others)

--- .bash/ruby.bash
+++ .bash/ruby.bash
@@ -5,10 +5,4 @@ if $HAS_TERMINAL; then
 
     [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 
-    # Use bundle exec to run these Rails commands
-    alias guard="bundle exec guard"
-    alias capify="bundle exec capify"
-    alias cap="bundle exec cap"
-    alias rake="bundle exec rake"
-
 fi
