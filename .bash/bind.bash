30b7d6afeacb4de42f2b8f3e127b397c478611bb
Dave James Miller <dave@davejamesmiller.com>
2014-05-24 00:58:50 +0100

    Fix warning when connecting via SSH

--- .bash/bind.bash
+++ .bash/bind.bash
@@ -1,3 +1,5 @@
-# Couldn't get these working in .inputrc
-bind '"\eOC":forward-word'
-bind '"\eOD":backward-word'
+if $HAS_TERMINAL; then
+    # Couldn't get these working in .inputrc
+    bind '"\eOC":forward-word'
+    bind '"\eOD":backward-word'
+fi
