7ecc4780abf678cfcd6b5851e4636c0ad30e872d
Dave James Miller <dave@davejamesmiller.com>
2014-05-24 00:54:27 +0100

    Remove default prompt message

--- .bash/010-config.bash
+++ .bash/010-config.bash
@@ -5,12 +5,13 @@ umask_root=022
 www_dir=
 
 # Default prompt is hostname with uppercase first letter with pink background
-if $WINDOWS; then
-    hostname="`hostname`"
-else
-    hostname="`hostname -s`"
-fi
-prompt_default="$(echo "${hostname:0:1}" | tr [a-z] [A-Z])${hostname:1}"
+#if $WINDOWS; then
+#    hostname="`hostname`"
+#else
+#    hostname="`hostname -s`"
+#fi
+#prompt_default="$(echo "${hostname:0:1}" | tr [a-z] [A-Z])${hostname:1}"
+prompt_default=
 prompt_type=
 
 # Load custom config options
