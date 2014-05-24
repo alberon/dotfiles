7c06db19781e18258c8825be911341a17755e8f1
Dave James Miller <dave@davejamesmiller.com>
2014-05-24 22:37:06 +0100

    Don't start tmux automatically
    Because setting the TERM var to 'screen' causes commands like 'htop' and 'less'
    to stop working on our CentOS 5 server, and I can't find a way to prevent an
    infinite loop without setting it. So instead start the connection with this command:
    
    tmux attach || tmux new -s default || bash -l

--- .bash/tmux.bash
+++ .bash/tmux.bash
@@ -1,3 +1,20 @@
+# Workaround for losing SSH agent connection when reconnecting tmux: update a
+# symlink to the socket each time we reconnect and use that as the socket in
+# every session. First we make sure there's a valid socket connecting us to the
+# agent and it's not already pointing to the symlink, and there's no existing
+# working symlink:
+link="$HOME/.ssh/ssh_auth_sock"
+if [ "$SSH_AUTH_SOCK" != "$link" -a -S "$SSH_AUTH_SOCK" -a ! -S "$link" ]; then
+    # We also check if the agent has any keys loaded - PuTTY will still open an
+    # agent connection even if we used password authentication
+    if ssh-add -l >/dev/null 2>&1; then
+        ln -nsf "$SSH_AUTH_SOCK" "$HOME/.ssh/ssh_auth_sock"
+    fi
+fi
+
+# Now that's done we can use the symlink for every session
+export SSH_AUTH_SOCK="$HOME/.ssh/ssh_auth_sock"
+
 # tmux attach
 alias tm='tmux attach'
 
