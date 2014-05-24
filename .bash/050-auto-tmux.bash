cd7b515656faa9fe2c1a25f6fa82ef9bf1d7c31b
Dave James Miller <dave@davejamesmiller.com>
2014-05-24 11:51:16 +0100

    Improved SSH agent handling for tmux

--- .bash/050-auto-tmux.bash
+++ .bash/050-auto-tmux.bash
@@ -1,3 +1,18 @@
+# Workaround for losing SSH agent connection when reconnecting tmux: update a
+# symlink to the socket each time we reconnect and use that as the socket in
+# every session. First we make sure there's a valid socket connecting us to the
+# agent and it's not already pointing to the symlink:
+if [ "$SSH_AUTH_SOCK" != "$HOME/.ssh/ssh_auth_sock" -a -S "$SSH_AUTH_SOCK" ]; then
+    # We also check if the agent has any keys loaded - PuTTY will still open an
+    # agent connection even if we used password authentication
+    if ssh-add -l >/dev/null 2>&1; then
+        ln -nsf "$SSH_AUTH_SOCK" "$HOME/.ssh/ssh_auth_sock"
+    fi
+fi
+
+# Now that's done we can use the symlink for every session
+export SSH_AUTH_SOCK="$HOME/.ssh/ssh_auth_sock"
+
 # Immediately switch to tmux if possible
 # This is better than changing the shell to tmux because it can be set to attach
 # to a running session if there is one
--- .bash/ssh.bash
+++
@@ -1,8 +0,0 @@
-# Workaround for losing SSH agent connection when reconnecting tmux:
-# update a symlink to the socket each time we reconnect and use that
-# as the socket in every session.
-# (Also see ~/.ssh/rc)
-
-if [ -S ~/.ssh/ssh_auth_sock ]; then
-    export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock
-fi
