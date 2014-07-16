d1ffe666212a8950321057665def2e4e53ca3dd7
Dave James Miller <dave@davejamesmiller.com>
2014-07-16 11:03:41 +0100

    Windows fixes - ssh and grep

--- .bash/ssh.bash
+++ .bash/ssh.bash
@@ -1,1 +1,10 @@
 alias sshstop='ssh -O stop'
+
+if $WINDOWS; then
+    ssh() {
+        touch ~/tmp/ssh-config-windows
+        chmod 700 ~/tmp/ssh-config-windows
+        command grep -v 'ControlPersist\|ControlPath' ~/.ssh/config > ~/tmp/ssh-config-windows
+        command ssh -F ~/tmp/ssh-config-windows "$@"
+    }
+fi
