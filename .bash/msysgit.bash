2a9c18f1aa2fb5d94f0277034562275317de64aa
Dave James Miller <dave@davejamesmiller.com>
2015-05-14 09:44:48 +0100

    Fix SSH in Msysgit on 64-bit PC

--- .bash/msysgit.bash
+++ .bash/msysgit.bash
@@ -1,3 +1,7 @@
 if [ -f /c/Program\ Files/PuTTY/plink.exe ]; then
     export GIT_SSH=/c/Program\ Files/PuTTY/plink.exe
+    alias ssh="/c/Program\ Files/PuTTY/plink.exe"
+elif [ -f /c/Program\ Files\ \(x86\)/PuTTY/plink.exe ]; then
+    export GIT_SSH=/c/Program\ Files\ \(x86\)/PuTTY/plink.exe
+    alias ssh="/c/Program\ Files\ \(x86\)/PuTTY/plink.exe"
 fi
