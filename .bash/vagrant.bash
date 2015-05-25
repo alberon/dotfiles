1dd78e26aa8445963e0d5c6084cec9d62a61b356
Dave James Miller <dave@davejamesmiller.com>
2015-05-25 22:01:42 +0100

    Add vagrant shortcuts

--- .bash/vagrant.bash
+++ .bash/vagrant.bash
@@ -1,7 +1,31 @@
 alias v=vagrant
 
 vagrant() {
-    if [ $# -eq 1 -a "$1" = "ssh" ]; then
+    # No parameters
+    if [ $# -eq 0 ]; then
+        command vagrant
+        echo
+        echo "Shortcuts:"
+        echo "     u    up"
+        echo "     p    provision"
+        echo "     s    ssh"
+        echo "     d    suspend (down)"
+        return
+    fi
+
+    # Parse the first parameter for shortcuts - because I'm lazy!
+    cmd="$1"
+    shift
+
+    case "$cmd" in
+        u) cmd=up        ;;
+        p) cmd=provision ;;
+        s) cmd=ssh       ;;
+        d) cmd=suspend   ;;
+    esac
+
+    # Special case for the 'ssh' command
+    if [ "$cmd" = "ssh" ]; then
         if [ -z "$TMUX" ]; then
             # Run tmux inside Vagrant (if available)
             command vagrant ssh -- -t 'which tmux >/dev/null 2>&1 && { tmux attach || tmux new -s default; } || bash -l'
@@ -15,7 +39,9 @@ vagrant() {
             # Run as normal
             command vagrant "$@"
         fi
-    else
-        command vagrant "$@"
+        return
     fi
+
+    # Other commands
+    command vagrant "$cmd" "$@"
 }
