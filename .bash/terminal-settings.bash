c8960e6b32eb6d8ef3b32e8819fc60ed0b4e4a15
Dave James Miller <dave@davejamesmiller.com>
2015-05-25 19:21:48 +0100

    Prevent error message when using MSysGit ssh

--- .bash/terminal-settings.bash
+++ .bash/terminal-settings.bash
@@ -4,7 +4,7 @@ if $HAS_TERMINAL && which stty >/dev/null 2>&1; then
 fi
 
 # Use 4 space tabs
-if $HAS_TERMINAL && which tabs >/dev/null 2>&1; then
+if $HAS_TERMINAL && which tabs >/dev/null 2>&1 && [ "$TERM" != "cygwin" ]; then
     # This outputs a blank line, but that doesn't seem preventable - if you
     # redirect to /dev/null it has no effect
     tabs -4
