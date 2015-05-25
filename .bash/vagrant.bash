52d2de03ee2bc41361a4a8e7eea4df5439715475
Dave James Miller <dave@davejamesmiller.com>
2015-05-25 19:18:05 +0100

    Default to running tmux inside vagrant ssh

--- .bash/vagrant.bash
+++ .bash/vagrant.bash
@@ -1,1 +1,10 @@
 alias v=vagrant
+
+vagrant() {
+    if [ $# -eq 1 -a "$1" = "ssh" ]; then
+        # Default to running tmux inside vagrant ssh
+        command vagrant ssh -- -t 'which tmux >/dev/null 2>&1 && { tmux attach || tmux new -s default; } || bash -l'
+    else
+        command vagrant "$@"
+    fi
+}
