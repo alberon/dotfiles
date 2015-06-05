c220c6d2260702917a01d8253616fbb7e9da899b
Dave James Miller <dave@davejamesmiller.com>
2015-06-05 22:12:56 +0100

    Merge the two Vagrant provisioning scripts to reduce the amount of output, and only provision when requested not every time

--- .vagrant.d/provision-dotfiles.sh
+++ .vagrant.d/provision-dotfiles.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
 set -eu
 
+export DEBIAN_FRONTEND=noninteractive
+
+# Install Git - required to install dotfiles
+if ! which git >/dev/null 2>&1; then
+    sudo apt-get update -y
+    sudo apt-get install -y git
+fi
+
+# Install tmux
+if ! which tmux >/dev/null 2>&1; then
+
+    # Ubuntu 12.04 Precise has an old version of tmux installed by default
+    if [ -f /etc/lsb-release ]; then
+        source /etc/lsb-release
+        if [ "$DISTRIB_RELEASE" = "12.04" ]; then
+            sudo apt-get install -y python-software-properties
+            sudo add-apt-repository -y ppa:pi-rho/dev 2>&1
+            # http://askubuntu.com/a/197532
+            sudo apt-get update -y -o Dir::Etc::sourcelist="sources.list.d/pi-rho-dev-precise.list" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"
+        fi
+    fi
+
+    sudo apt-get install -y tmux
+
+fi
+
+# Install dotfiles
 if [ ! -d .git ]; then
 
     # Based on https://djm.me/cfg but non-interactive
