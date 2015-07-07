0aff92336eadc3dea19313de32ab5a6b22eeb778
Dave James Miller <dave@davejamesmiller.com>
2015-07-07 15:01:55 +0100

    Vagrant - Switch to alberon dotfiles

--- .vagrant.d/provision-dotfiles.sh
+++ .vagrant.d/provision-dotfiles.sh
@@ -130,8 +130,8 @@ if [ ! -d .git ]; then
     # Based on https://djm.me/cfg but quiet and non-interactive
     echo "Installing dotfiles in $HOME..."
     git init -q
-    git remote add origin git://github.com/davejamesmiller/dotfiles.git
-    git remote set-url --push origin git@github.com:davejamesmiller/dotfiles.git
+    git remote add origin git://github.com/alberon/dotfiles.git
+    git remote set-url --push origin git@github.com:alberon/dotfiles.git
     git fetch -q origin
     rm -f .bashrc .bash_profile
     git checkout origin/master -b master >/dev/null 2>&1
@@ -149,8 +149,8 @@ sudo -s <<END
         echo "Installing dotfiles in $(echo ~root)..."
         cd ~root
         git init -q
-        git remote add origin git://github.com/davejamesmiller/dotfiles.git
-        git remote set-url --push origin git@github.com:davejamesmiller/dotfiles.git
+        git remote add origin git://github.com/alberon/dotfiles.git
+        git remote set-url --push origin git@github.com:alberon/dotfiles.git
         git fetch -q origin
         rm -f .bashrc .bash_profile
         git checkout origin/master -b master >/dev/null 2>&1
@@ -160,6 +160,6 @@ sudo -s <<END
 END
 
 # Allow access to the root user
-if [ -f ~/.ssh/davejamesmiller.pub ]; then
-    sudo cp -f ~/.ssh/davejamesmiller.pub ~root/.ssh/authorized_keys
+if [ -f ~/.ssh/alberon.pub ]; then
+    sudo cp -f ~/.ssh/alberon.pub ~root/.ssh/authorized_keys
 fi
