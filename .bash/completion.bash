ffa00876763e3e8b8fb9cc1e7898caf5e883f976
Dave James Miller <dave@davejamesmiller.com>
2015-07-11 17:36:02 +0100

    'z' script - tab-completion, and skip non-executable files in list

--- .bash/completion.bash
+++ .bash/completion.bash
@@ -5,3 +5,44 @@ fi
 
 # Don't tab-complete an empty line - there's not really any use for it
 shopt -s no_empty_cmd_completion
+
+# Custom completions
+# complete -C "z --bash-completion" z
+
+complete -F _z_completion z
+
+_z_completion() {
+    # Locate the scripts directory
+    if ! root="$(findup -d scripts)"; then
+        return
+    fi
+
+    scripts="$root/scripts"
+
+    # Subdirectory?
+    for ((i = 1; i < $COMP_CWORD; i++)); do
+        scripts="$scripts/${COMP_WORDS[$i]}"
+    done
+
+    if [ ! -d "$scripts" ]; then
+        return
+    fi
+
+    # List matching scripts
+    COMPREPLY=()
+
+    for file in "$scripts/${COMP_WORDS[$COMP_CWORD]}"*; do
+        name="$(basename "$file")" # Remove path
+        name="${name%%.*}"         # Remove extension
+        if [ ! -x "$file" ]; then
+            : # Skip non-executable files
+        elif [ "${name^^}" = "README" ]; then
+            : # Skip readme files
+        elif [[ "$name" == *" "* ]]; then
+            # Spaces in the name
+            COMPREPLY+=("'$name'")
+        else
+            COMPREPLY+=("$name")
+        fi
+    done
+}
