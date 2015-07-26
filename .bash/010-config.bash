c4b1531202c6aa4986f0245eb1c4cb2b129ffc15
Dave James Miller <dave@davejamesmiller.com>
2015-07-26 18:40:37 +0100

    Detect Cygwin documents path for 'cw', and simplify cfg-install

--- .bash/010-config.bash
+++ .bash/010-config.bash
@@ -1,7 +1,8 @@
 # Defaults
-enable_sudo=0
-umask_user=007
+prompt_default=
+prompt_type=
 umask_root=022
+umask_user=007
 www_dir=
 
 # Default prompt is hostname with uppercase first letter with pink background
@@ -11,8 +12,6 @@ www_dir=
 #    hostname="`hostname -s`"
 #fi
 #prompt_default="$(echo "${hostname:0:1}" | tr [a-z] [A-Z])${hostname:1}"
-prompt_default=
-prompt_type=
 
 # Load custom config options
 if [ -f ~/.bashrc_config ]; then
