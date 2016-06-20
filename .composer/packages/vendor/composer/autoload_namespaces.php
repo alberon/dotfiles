9d3b88852994263f03670f0602066007817c97f5
Dave James Miller <dave@davejamesmiller.com>
2016-06-20 10:27:54 +0100

    Install SensioLabs Security Checker

--- .composer/packages/vendor/composer/autoload_namespaces.php
+++ .composer/packages/vendor/composer/autoload_namespaces.php
@@ -9,6 +9,7 @@ return array(
     'phpDocumentor' => array($vendorDir . '/phpdocumentor/reflection-docblock/src'),
     'cli' => array($vendorDir . '/wp-cli/php-cli-tools/lib'),
     'WP_CLI' => array($vendorDir . '/wp-cli/wp-cli/php'),
+    'SensioLabs\\Security' => array($vendorDir . '/sensiolabs/security-checker'),
     'Requests' => array($vendorDir . '/rmccue/requests/library'),
     'Prophecy\\' => array($vendorDir . '/phpspec/prophecy/src'),
     'Oxymel' => array($vendorDir . '/nb/oxymel'),
