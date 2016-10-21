a3bbbdb885fdf681875ee595b2ecb97b2c01e155
Dave James Miller <dave@davejamesmiller.com>
2016-10-21 10:34:03 +0100

    Fix tab completion on CentOS 5 + 6

--- .bash/completion.bash
+++ .bash/completion.bash
@@ -3,5 +3,7 @@ if [ -f /etc/bash_completion ]; then
     source /etc/bash_completion
 fi
 
+source ~/.bash_completion
+
 # Don't tab-complete an empty line - there's not really any use for it
 shopt -s no_empty_cmd_completion
