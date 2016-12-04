3b6cae183e65363d829b8ae3daab68e6bf2db973
Dave James Miller <dave@davejamesmiller.com>
2016-12-04 09:27:32 +0000

    Separate a=awe, p=gulp to avoid confusion

--- .bash/build-tools.bash
+++ .bash/build-tools.bash
@@ -1,15 +1,3 @@
-a()
-{
-    if findup -f awe.yaml; then
-        # If awe.yaml exists in the project, use Awe
-        awe "$@"
-    else
-        # Otherwise assume it's Gulp (the Gulpfile may have several names, e.g.
-        # Gulpfile.js, gulpfile.babel.js, etc.
-        gulp "$@"
-    fi
-}
-
 gulp()
 {
     # Gulper automatically reloads gulp when the Gulpfile is modified
@@ -20,7 +8,9 @@ gulp()
     fi
 }
 
+alias a='awe'
 alias aweup='sudo npm update -g awe'
+alias p='gulp'
 
 # Use the development version of Awe in preference to the stable version
 PATH="$HOME/awe/bin:$PATH"
