29d3821b903860555a7760e73445234521885c02
Dave James Miller <dave@davejamesmiller.com>
2017-05-13 19:31:25 +0100

    More work on Docker configuration

--- .bash/docker.bash
+++ .bash/docker.bash
@@ -1,20 +1,92 @@
-alias d='winpty docker'
-alias dc='winpty docker-compose'
-alias dm='winpty docker-machine'
-
-dsh()
+# Windows support
+docker()
 {
-    # Determine the image ID, build it if necessary
-    image="${1:-.}"
+    if $WINDOWS; then
+        winpty docker $(cygpath -w -- "$@")
+    else
+        docker "$@"
+    fi
+}
 
-    if [ "$image" = "." ]; then
-        image="$(docker build -q .)"
+alias docker-compose='winpty docker-compose'
+alias docker-machine='winpty docker-machine'
 
-        if [ -z "$image" ]; then
-            return
-        fi
+# Shorthand
+alias d='docker'
+alias db='docker build'
+alias dc='docker-compose'
+alias dm='docker-machine'
+alias dr='docker run'
+
+# Clean up stopped containers and dangling (untagged) images
+dclean()
+{
+    echo "Cleaning stopped containers..."
+    containers="$(docker ps -qf status=exited)"
+
+    if [ -n "$containers" ]; then
+        docker rm $containers
     fi
 
+    echo "Cleaning dangling images..."
+    images="$(docker images -qf dangling=true)"
+
+    if [ -n "$images" ]; then
+        docker rmi $images
+    fi
+
+    echo "Done."
+}
+
+# Environment
+denv()
+{
+    echo "Switching Docker environment..."
+    cmd="$(docker-machine env "${1:-Docker}")" || return
+    eval "$cmd"
+    echo "Done."
+}
+
+# Kill all containers
+dkillall()
+{
+    containers="$(docker ps -q)"
+
+    if [ -n "$containers" ]; then
+        docker kill $containers
+    fi
+}
+
+# Init
+dinit()
+{
+    docker-machine create --driver virtualbox "${1:-Docker}"
+    denv "${1:-Docker}"
+}
+
+# Remove
+dremove()
+{
+    docker-machine rm "$DOCKER_MACHINE_NAME"
+}
+
+# Resume
+dresume()
+{
+    # http://stackoverflow.com/a/37886136/167815
+    container="$(docker ps -qlf status=exited)"
+
+    if [ -n "$container" ]; then
+        docker start -ai "$container"
+    else
+        echo "No stopped images found." >&2
+        return 1
+    fi
+}
+
+# Shell
+dsh()
+{
     # Set up SSH agent forwarding
     if [ -n "$SSH_AUTH_SOCK" ]; then
         opt="--volume \$SSH_AUTH_SOCK:/tmp/ssh-agent --env SSH_AUTH_SOCK=/tmp/ssh-agent"
@@ -22,30 +94,25 @@ dsh()
         opt=
     fi
 
-    # Build the command to run Bash on the specified image
-    local cmd="docker run $opt -it $image ${2:-bash}"
+    # Build the command to run a shell on the specified image
+    local cmd="docker run $opt -it --entrypoint '${2:-/bin/bash}' '${1:-ubuntu}'"
 
     # If using Windows, we need to connect to the Docker VM first
     if $WINDOWS; then
         # -A = Enable agent forwarding, -t = Force TTY allocation
-        winpty docker-machine ssh $DOCKER_MACHINE_NAME -At "$cmd"
+        dssh "$DOCKER_MACHINE_NAME" -At "$cmd"
     else
         # Untested...
         $cmd
     fi
 }
 
-dme()
+# SSH to docker-machine
+dssh()
 {
-    echo "Switching Docker environment..."
-    winpty eval $(docker-machine env "${1:-default}")
-}
-
-winpty()
-{
-    if $WINDOWS; then
-        "$HOME/opt/winpty/bin/winpty.exe" "$@"
-    else
-        "$@"
-    fi
+    # This avoids using 'docker-machine ssh' which breaks formatting in Cygwin
+    machine="${1:-$DOCKER_MACHINE_NAME}"
+    shift
+    ip="$(docker-machine ip "$machine")" || return
+    ssh -i "$HOME/.docker/machine/machines/$machine/id_rsa" "docker@$ip" "$@"
 }
