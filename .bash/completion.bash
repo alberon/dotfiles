571bc322b9ce249b0296c2d8d5097f9c2bef7384
Alberon Ltd <info@alberon.co.uk>
2021-10-21 22:37:55 +0100

    Move Bash completion scripts to their default location & remove custom loader

--- .bash/completion.bash
+++ .bash/completion.bash
@@ -10,7 +10,5 @@ if [ -f /etc/bash_completion ]; then
     source /etc/bash_completion
 fi
 
-source ~/.bash_completion
-
 # Don't tab-complete an empty line - there's not really any use for it
 shopt -s no_empty_cmd_completion
